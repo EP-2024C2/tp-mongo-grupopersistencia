cf604fd9b238193b914d5f9a0c88fa6e6e60ab5b
AlexisChebeste <alexispaulino.chebeste@estudiantes.unahur.edu.ar>
2024-11-18 23:29:06 -0300

    initData

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes/index.routes');
 const { mongoose, connectToDatabase } = require('./db/mongo.db');
+const initData = require('./init/data')
 
 const PORT = process.env.PORT || 3000 
 
@@ -13,5 +14,6 @@ app.use(routes.fabricantesRoute)
 
 app.listen(PORT, async() => {
     await connectToDatabase();
+    await initData();
     console.log(` \u{1F680} Aplicación iniciada en el puerto ${PORT} - http://localhost:${PORT}/`)
 })
--- src/db/mongo.db.js
+++ src/db/mongo.db.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const MONGO_URL = process.env.MONGO_URL ?? "mongodb://admin:admin1234@localhost:27017/producto?authSource=admin";
 
-const initData = require('../init/data')
+
 
 async function connectToDatabase () {
   try {
     await mongoose.connect(MONGO_URL);
     console.log("Conexión a mongo realizada con exito")
-    await initData();
+
   }
   catch (err) {
     console.error('Error al conectarse a mongo', err.message)
